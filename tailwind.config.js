d7a33ea0a480aa8e34aa46618c94827ea6f5315b
Minh Duc Do <minhducdo@users.noreply.github.com>
2026-03-06 23:24:23 +0800

    fix: migrate tailwind.config.js to ESM

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import tailwindAnimate from 'tailwindcss-animate';
+
+** @type {import('tailwindcss').Config} */
+export default {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,10 +11,10 @@ module.exports = {
       colors: {
         background: '#0f112a',
         foreground: '#cbd5e1',
-      }
+      },
     },
   },
   plugins: [
-    require('tailwindcss-animate'),
+    tailwindAnimate,
   ],
 }
